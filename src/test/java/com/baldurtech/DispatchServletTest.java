fa9d590fe5d77111c46878f65508347be6d57624
baijiayong <jiayongbai@gmail.com>
2014-08-11 11:26:44 +0800

    重构 DispatchServletTest 测试

--- src/test/java/com/baldurtech/DispatchServletTest.java
+++ src/test/java/com/baldurtech/DispatchServletTest.java
@@ -3,14 +3,13 @@ package com.baldurtech;
 import com.baldurtech.unit.MiniatureSpiceTestCase;
 
 public class DispatchServletTest extends MiniatureSpiceTestCase {
-    public void test_uri_contact_show_应该由ContactAction来处理() {
-        DispatchServlet dispatchServlet = new DispatchServlet();
-        
-        assertEquals("com.baldurtech.ContactAction",dispatchServlet.getActionClassNameByUri("/contact/show"));
+    DispatchServlet dispatchServlet = new DispatchServlet();
+    String contactActionClassName = "com.baldurtech.ContactAction";
+    
+    public void test_uri_contact_show_应该由ContactAction来处理() { 
+        assertEquals(contactActionClassName,dispatchServlet.getActionClassNameByUri("/contact/show"));
     }
     public void test_uri_contact_应该由ContactAction来处理() {
-        DispatchServlet dispatchServlet = new DispatchServlet();
-        
-        assertEquals("com.baldurtech.ContactAction",dispatchServlet.getActionClassNameByUri("/contact"));
+        assertEquals(contactActionClassName,dispatchServlet.getActionClassNameByUri("/contact"));
     }
 }
