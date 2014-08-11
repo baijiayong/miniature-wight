3460fe63bfa50d2c03f235464fbbe099766d56a8
baijiayong <jiayongbai@gmail.com>
2014-08-11 14:33:15 +0800

    抽取用斜线分割字符串的方法为 splitBySlash 方法

--- src/main/java/com/baldurtech/DispatchServlet.java
+++ src/main/java/com/baldurtech/DispatchServlet.java
@@ -10,23 +10,30 @@ public class DispatchServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {       
         resp.getWriter().println(req.getRequestURI());
     }
+    
     public String defaultPackageName = "com.baldurtech";
     public String defaultSuffix = ".jsp";
     
     public String getActionClassNameByUri(String uri) {
-        String[] uriParts = uri.split("/");
+        String[] uriParts = splitBySlash(uri);
         Integer indexOfActionClassName = 1;
         String actionClassName = capitalize(uriParts[indexOfActionClassName]).replace(defaultSuffix,"");
         return defaultPackageName + "." + actionClassName + "Action";
     }
+    
     public String getMethodNameByUri(String uri) {
-        String[] uriParts = uri.split("/");
+        String[] uriParts = splitBySlash(uri);
         Integer indexOfMethodName = 2;
         if(uriParts.length <= indexOfMethodName) {
             return "index";
         }
         return uriParts[indexOfMethodName].replace(defaultSuffix,"");
     }
+    
+    public String[] splitBySlash(String uri) {
+        return uri.split("/");
+    }
+    
     public String capitalize(String str) {
         return str.substring(0,1).toUpperCase() + str.substring(1);
     }
