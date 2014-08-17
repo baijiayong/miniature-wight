298ef4e9314f66ed5ea8ec8ab21634f257d44da7
baijiayong <jiayongbai@gmail.com>
2014-08-17 14:45:37 +0800

    重构了 DispatchServlet

--- src/main/java/com/baldurtech/DispatchServlet.java
+++ src/main/java/com/baldurtech/DispatchServlet.java
@@ -7,16 +7,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DispatchServlet {
+
     public void doget(HttpServletRequest request, HttpServletResponse response)
         throws ServletException,IOException 
     {
         
     }
+    
+    public String defaultPackageName = "com.baldurtech";
     public String getClassNameByUri(String uri) {
         String[] uriParts = uri.split("/");
         Integer indexOfActionClassName = 1;
-        String actionClassName = uriParts[indexOfActionClassName];
-        actionClassName = actionClassName.substring(0,1).toUpperCase() + actionClassName.substring(1);
-        return "com.baldurtech." + actionClassName + "Action";
+        String actionClassName =capitalize(uriParts[indexOfActionClassName]);
+        return defaultPackageName + "." + actionClassName + "Action";
+    }
+    
+    public String capitalize(String str) {
+        return str.substring(0,1).toUpperCase() + str.substring(1);
     }
 }   
